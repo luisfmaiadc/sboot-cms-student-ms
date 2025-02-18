dbc04479ccfd6b9c5c321dfea3a8246bebca5586
luisfmaiadc <lf.mcosta23@gmail.com>
2025-02-18 17:21:47 -0300

    Inicialização do versionamento do microsserviço.

---
+++ src/main/java/com/portfolio/luisfmdc/sboot_cms_student_ms/SbootCmsStudentMsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_cms_student_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootCmsStudentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootCmsStudentMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/sboot_cms_student_ms/SbootCmsStudentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_cms_student_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootCmsStudentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
